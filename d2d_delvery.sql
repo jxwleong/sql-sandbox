790eaa570d7dbbe951d01efbe4b699b5a66066e6
JLXW <jason.xie.wei.leong@gmail.com>
2022-08-27 21:02:21 +0800

    Added other dimension tables

--- d2d_delvery.sql
+++ d2d_delvery.sql
@@ -1,5 +1,7 @@
+-- Online SQL Editor to Run SQL Online.
+-- Use the editor to create new tables, insert data and all other SQL operations.
+  
 DROP TABLE Customers;
-DROP TABLE DeliveryFactTable;
 DROP TABLE Orders;
 DROP TABLE Shippings;
 
@@ -12,4 +14,47 @@ CREATE TABLE DeliveryFactTable(
   ORDER_AMT INT NOT NULL,
   CUST_LOCATION_ID varchar(100) NOT NULL,
   REST_LOCATION_ID varchar(100) NOT NULL
+);
+
+CREATE TABLE RestaurantDimensionTable( 
+  RES_NAME varchar(100) NOT NULL,
+  RES_TEL varchar(100) NOT NULL,
+  RES_CATEGORY varchar(100) NOT NULL
+);
+
+CREATE TABLE CustomerDimensionTable( 
+  CUST_NAME varchar(100) NOT NULL,
+  CUST_TEL varchar(100) NOT NULL,
+  CUST_PWORD varchar(100) NOT NULL
+);
+
+CREATE TABLE DriverDimensionTable( 
+  DRIVER_NAME varchar(100) NOT NULL,
+  DRIVER_TEL varchar(100) NOT NULL,
+  DRIVER_IC_NUM varchar(100) NOT NULL,
+  DRIVER_CAR_BRAND varchar(100) NOT NULL,
+  DRIVER_CAR_NUMPLATE varchar(100) NOT NULL
+);
+
+CREATE TABLE LocationDimensionTable( 
+  LocationID varchar(100) NOT NULL,
+  State varchar(100) NOT NULL,
+  City varchar(100) NOT NULL,
+  Zip INT NOT NULL,
+  Street varchar(100) NOT NULL
+);
+
+CREATE TABLE OrderDimensionTable( 
+  ORDER_STATUS varchar(100) NOT NULL,
+  ORDER_AMT varchar(100) NOT NULL,
+  ORDER_YEAR varchar(100) NOT NULL,
+  ORDER_MONTH INT NOT NULL,
+  ORDER_DAY INT NOT NULL,
+  ORDER_TIME INT NOT NULL
+);
+
+CREATE TABLE PaymentDimensionTable( 
+  PROMO_CODE INT NOT NULL,
+  PAYMENT_METHOD varchar(100) NOT NULL,
+  PAYMENT_AMOUNT INT NOT NULL
 );
